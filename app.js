451d229c5b524bfcc76a3aa6dbf67a8cd60155b2
Arturo Padilla Calvo <turopadilla93@gmail.com>
2018-05-17 00:09:10 -0600

    Add node requirements and project execution code

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
+
+var routes = require('./index');
+
+var app = express();
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+app.listen(3000,function(){
+    console.log("Listening on PORT 3000");
+});
