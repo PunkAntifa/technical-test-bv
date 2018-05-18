7ec880dcab6bee889232c4cfea63c5fd57286756
Arturo Padilla Calvo <turopadilla93@gmail.com>
2018-05-17 21:38:31 -0600

    Add feature to get genres from database and put them on select list

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express     = require("express");
-var router         = express.Router();
+var express = require("express");
+var router  = express.Router();
 
 //Sqlite database
 const sqlite3 = require('sqlite3').verbose();
@@ -12,22 +12,41 @@ const db = new sqlite3.Database('./db/bvde.db', sqlite3.OPEN_READWRITE, (err) =>
     console.log('Connected to the bvde database.');
 });
 
+const db_get_genres_query = 'SELECT name as Name from Genres;'
+var genres = [];
+
 //Setting home page
 router.get('/',function(req, res, next){
-    var genres = ["Pop", "Rock", "Salsa"];
-    res.render('index', {output: genres});
+    //Prepare genres data in json formar
+    let data = [];
+    for (var i = 0; i < genres.length; i++){
+        data.push({'value': genres[i], 'text': genres[i]});
+    }
+    //Render the index view with data to create the <option> tags
+    res.render('index', {data});
 });
 
 router.get('/search/:arguments', function(req, res, next){
-    //console.log(req.params.arguments);
     res.render('search', {output: req.params.arguments});
 });
 
 router.post('/search', function(req, res, next){
     var artist = req.body.artist;
     var songTitle = req.body.songTitle;
-    var genre = req.body.genre;
+    var genre = req.body.genres;
     res.redirect('/search/' + artist + '&' + songTitle + '&' + genre);
 });
+
+//DB query - Get all genres and set data in genres array
+db.serialize(() => {
+    db.all(db_get_genres_query, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            genres.push(row.Name);
+        });
+    });
+}) 
 
 module.exports = router;
